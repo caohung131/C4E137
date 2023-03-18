448ac9ab6efda69989649ec297d429e30f20ed46
caohung131 <caohung131@gmail.com>
2023-03-18 14:57:43 +0700

    add

---
+++ Lesson/Lesson6/app.js
@@ -0,0 +1,61 @@
+    let x = true;
+    let y = true;
+    let z = false;
+
+    var kq1= (x && y && z);
+    var kq2= (x && y && !z);
+    var kq3= ((x && y) || z);
+    var kq4= ((x && y) || !z);
+    var kq5= x && (y || z);
+    var kq6= x && !(y || z);
+    var kq7= x && (y || !z);
+    var kq8= x && (!y || z);
+    console.log("Kết quả 1 là :" +kq1);
+    console.log("Kết quả 2 là :" +kq2);
+    console.log("Kết quả 3 là :" +kq3);
+    console.log("Kết quả 4 là :" +kq4);
+    console.log("Kết quả 5 là :" +kq5);
+    console.log("Kết quả 6 là :" +kq6);
+    console.log("Kết quả 7 là :" +kq7);
+    console.log("Kết quả 8 là :" +kq8);
+    d = '40';
+    console.log("Kết quả 8 là :" +d-90);
+    
+2.
+    30
+    20%3 = 2
+    AAA  BBB
+    false
+
+
+    true
+    false
+    false
+
+3.
+ 1.60
+ 2.-10/30 = -0.33333333333
+ 3. 10 - 60 = 50
+ 4. '40' + -60 - 30 = 50
+ 5. 6040
+ 6. 40102030
+ 7. 40102030
+ 8. 4010 - 20 + 30 = 4020
+ 9. 
+
+
+
+5.
+ 24
+ 1234
+ -1-3 + "4" = "-44"
+ -1-3 + -1 = -5
+ -1-3 -3 + "4" = "-74"
+ "24"- 4 = 20
+ "12" -"34" = -22
+ 3 + 8 = 11
+ "-24"
+ "-2"-4 = -6
+ "-24"
+ 
+
